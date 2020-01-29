1cdc3450871fd1dcde22c56671ca562311831b18
Anthony Slater <aslater1@myseneca.ca>
2020-01-28 22:56:26 -0500

    add score and runs

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ function countDown(timeleft) {
         endGame();
       }, 300);
     }
-  }, 400);
+  }, 650);
 }
 
 function startGame() {
@@ -40,10 +40,48 @@ let options = [
   { name: "Scissors", wins: "Paper" }
 ];
 
+var your_score = 0;
+var comp_score = 0;
+var ties = 0;
+var run = [];
+
 function getResult(choice) {
   let c = Math.floor(Math.random() * 3);
   let c_pick = options[c].name;
   let bad = options[c].wins;
   let result = choice.localeCompare(bad) === 0 ? "lose" : (choice.localeCompare(c_pick) === 0 ? "Tie" : "Win");
-  displayMsg(`you picked ${choice} and comp picks ${c_pick} so you ${result}`);
+  if(choice.localeCompare(bad) === 0){
+    $("#comp_score").text(++comp_score);
+  } else if (choice.localeCompare(c_pick)==0){
+    $("#ties").text(++ties);
+  } else {
+    $("#your_score").text(++your_score);
+  }
+  displayMsg(`You picked ${choice} and I picked ${c_pick} so you ${result}`);
+  addToRun(result);
 }
+
+function addToRun(result){
+  run.push(result);
+  $("#run").text(run);
+
+  if(run.length==5){
+    var wins = 0;
+    run.forEach(result => {
+      if(result.localeCompare("Win")==0)
+        wins++;     
+    });
+    if (wins == 5)
+      displayMsg("FIVE IN WHOA!");
+    else 
+      run.shift();   
+  }
+
+}
+
+$(document).ready(function(){
+  $("#your_score, #comp_score, #ties").text(0);
+});
+
+// keep a tally of win runs
+// wins in a row
